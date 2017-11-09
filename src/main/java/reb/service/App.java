9f688525b3b31e532a70a7fe2026ac916529698b
Richard Edvin Borge <r.e.borge@ceres.no>
2017-11-09 19:33:58 +0100

    Initial

---
+++ src/main/java/reb/service/App.java
@@ -0,0 +1,13 @@
+package reb.service;
+
+import static spark.Spark.*;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+    public static void main( String[] args ) {
+        get("/hello", (req, res) -> "Hello World");
+    }
+}
