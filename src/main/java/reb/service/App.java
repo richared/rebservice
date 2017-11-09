d9b3fbe651f59327d9b12d5ed111df4f8ec79898
Richard Edvin Borge <r.e.borge@ceres.no>
2017-11-09 20:06:37 +0100

    Oppdatere #RS-1 Fixed

--- src/main/java/reb/service/App.java
+++ src/main/java/reb/service/App.java
@@ -8,6 +8,6 @@ import static spark.Spark.*;
  */
 public class App {
     public static void main( String[] args ) {
-        get("/hello", (req, res) -> "Heisann!");
+        get("/hello", (req, res) -> "Heisann!!");
     }
 }
