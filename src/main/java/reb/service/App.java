f87a909905ddebd39de8af1b5bf609ed9bb339d5
Richard Edvin Borge <r.e.borge@ceres.no>
2017-11-09 20:03:56 +0100

    Oppdatere #RS-1 comment En norsk hilsen

--- src/main/java/reb/service/App.java
+++ src/main/java/reb/service/App.java
@@ -8,6 +8,6 @@ import static spark.Spark.*;
  */
 public class App {
     public static void main( String[] args ) {
-        get("/hello", (req, res) -> "Hello World");
+        get("/hello", (req, res) -> "Heisann!");
     }
 }
